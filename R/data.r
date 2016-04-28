2fc7633443db550f97ce7b909b989ed0a2a76331
hadley <h.wickham@gmail.com>
2016-04-28 10:28:14 +0100

    Remove extraneous ws

--- R/data.r
+++ R/data.r
@@ -17,11 +17,11 @@
 #' \item ArrTime actual arrival time in hhmm format
 #' \item TaxiOut numeric variable of the taxi out time in minutes
 #' \item TaxiIn numeric variable of the taxi in time in minutes
-#' \item  CarrierDelay	Carrier Delay, in Minutes
-#' \item  WeatherDelay	Weather Delay, in Minutes
-#' \item  NASDelay	National Air System Delay, in Minutes
-#' \item  SecurityDelay	Security Delay, in Minutes
-#' \item  LateAircraftDelay	Late Aircraft Delay, in Minutes
+#' \item CarrierDelay	Carrier Delay, in Minutes
+#' \item WeatherDelay	Weather Delay, in Minutes
+#' \item NASDelay	National Air System Delay, in Minutes
+#' \item SecurityDelay	Security Delay, in Minutes
+#' \item LateAircraftDelay	Late Aircraft Delay, in Minutes
 #' }
 #' @examples
 #' library(ggplot2)
